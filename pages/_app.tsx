be4eaadf595c8753297c58ffcce4e07cccd302dd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-07-22 12:45:49 +0300

    Add initial Next.js project files

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Index = (): JSX.Element => <></>;
+
+export default Index;

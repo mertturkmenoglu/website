e013d8d6f917091b40aba51108940f0faafdd139
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-31 01:53:48 +0300

    Update index page markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import React from 'react';
 import AboutBanner from '../components/AboutBanner';
@@ -10,25 +12,38 @@ import TopNavigation from '../components/TopNavigation';
 import VerticalContactList from '../components/VerticalContactList';
 import Work from '../components/Work';
 
-const Index = (): JSX.Element => (
-  <main>
-    <VerticalContactList className="fixed bottom-8 left-8 invisible md:visible" />
-    <div className="h-screen flex flex-col">
-      <TopNavigation className="pt-8" />
-      <Hero className="flex flex-col items-center mt-32 text-center" />
-      <div className="flex justify-center items-end flex-grow">
-        <a href="/#about-banner">
-          <ChevronDownIcon className="w-16 h-16 animate-bounce text-purple-600 mb-8" />
-        </a>
-      </div>
-    </div>
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Mert Turkmenoglu</title>
+      </Head>
 
-    <AboutBanner className="w-full from-indigo-600 to-purple-600 bg-gradient-to-bl" />
-    <Work className="mt-32" />
-    <Projects className="mt-32" />
-    <Contact className="mt-48" />
-    <Footer className="w-full from-indigo-600 to-purple-600 bg-gradient-to-bl mt-32 md:mt-64" />
-  </main>
-);
+      <header>
+        <TopNavigation className="pt-8" />
+      </header>
+
+      <main>
+        <VerticalContactList className="fixed bottom-8 left-8 hidden md:visible" />
+        <div className="h-screen flex flex-col">
+          
+          <Hero className="flex flex-col items-center mt-32 text-center" />
+          <div className="flex justify-center items-end flex-grow">
+            <a href="/#about-banner">
+              <ChevronDownIcon className="w-16 h-16 animate-bounce text-purple-600 mb-8" />
+            </a>
+          </div>
+        </div>
+
+        <AboutBanner className="w-full from-indigo-600 to-purple-600 bg-gradient-to-bl" />
+        <Work className="mt-32" />
+        <Projects className="mt-32" />
+        <Contact className="mt-48" />
+      </main>
+
+      <Footer className="w-full from-indigo-600 to-purple-600 bg-gradient-to-bl mt-32 md:mt-64" />
+    </>
+  );
+};
 
 export default Index;

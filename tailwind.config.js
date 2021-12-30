8b84b9955e43cda75e117268225e3cbe58bb9e47
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-31 01:47:53 +0300

    Update project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     colors: {
       ...colors,
     },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

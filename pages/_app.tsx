c23f53bb86cf0897a4b6d8c50e7b223adb68c31c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-05-10 17:01:48 +0300

    Delete old components

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/globals.css';
-
-import type { AppProps } from 'next/app';
-
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
-
-export default MyApp;

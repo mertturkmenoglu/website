27a997dbd81cd6f0cd7be066f11f12475c33f8f5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-04 13:55:56 +0300

    Add read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,9 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     mdx({
-
-    })
+    }),
   ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });

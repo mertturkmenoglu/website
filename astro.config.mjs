e237121e46ab7d4303cb573b12f1990e8ae5d182
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-05 00:44:49 +0300

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,34 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import rehypeExternalLinks from "rehype-external-links";
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mertturkmenoglu.com',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx(), sitemap()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    mdx(),
+    sitemap(),
+  ],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [rehypeExternalLinks],
+    remarkPlugins: [
+      remarkReadingTime,
+      [remarkToc, { heading: "contents"} ],
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeExternalLinks, { content: { type: 'text', value: ' ↩' } }],
+      [rehypeAutolinkHeadings, { behavior: 'append' }]
+    ],
   },
+  experimental: {
+    viewTransitions: true,
+  }
 });

96ac897ff47bcf387010477a11b74687efbb8645
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-25 19:22:53 +0300

    reformat tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'primary': '#be185d',
-        'midnight': '#1a1a1a',
-        'accent': '#ffff00',
+        primary: "#be185d",
+        midnight: "#1a1a1a",
+        accent: "#ffff00",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
